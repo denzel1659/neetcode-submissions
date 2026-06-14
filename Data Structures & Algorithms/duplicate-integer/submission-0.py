ba42d9dfd5531810270f57aa11407626a81dd108
Denzel <35877108+denzel1659@users.noreply.github.com>
2026-06-13 22:08:49 -0700

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        tracker = {}
+        for num in nums:
+            if num in tracker:
+                return True
+            else:
+                tracker[num] = 1
+        return False
